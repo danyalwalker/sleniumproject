ca1540bb3fb9709c69f8c261dfa8de0851a4990e
Daniel <danielwalker.ny@gmail.com>
2021-05-24 16:55:21 -0400

    Practice to commit by adding a new class

---
+++ src/test/java/com/cybertek/tests/Practice/MakingCommitsInSelenium.java
@@ -0,0 +1,7 @@
+package com.cybertek.tests.Practice;
+
+public class MakingCommitsInSelenium {
+    public static void main(String[] args) {
+        System.out.println("I am practicing github by adding selenium files ");
+    }
+}
