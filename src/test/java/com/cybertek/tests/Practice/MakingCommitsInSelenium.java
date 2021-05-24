e9e31f0d416ab3c03f07687f992f210823e4e8d9
Daniel <danielwalker.ny@gmail.com>
2021-05-24 17:12:13 -0400

    local repo published on github

--- src/test/java/com/cybertek/tests/Practice/MakingCommitsInSelenium.java
+++ src/test/java/com/cybertek/tests/Practice/MakingCommitsInSelenium.java
@@ -3,5 +3,6 @@ package com.cybertek.tests.Practice;
 public class MakingCommitsInSelenium {
     public static void main(String[] args) {
         System.out.println("I am practicing github by adding selenium files ");
+        System.out.println("local repo is published to github");
     }
 }
