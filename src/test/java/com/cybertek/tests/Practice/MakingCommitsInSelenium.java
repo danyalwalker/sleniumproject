a2dae85bdc1e8fb9dab2e8dfc4d3f2c757640923
Daniel <danielwalker.ny@gmail.com>
2021-05-25 13:17:44 -0400

    add more class work

--- src/test/java/com/cybertek/tests/Practice/MakingCommitsInSelenium.java
+++ src/test/java/com/cybertek/tests/Practice/MakingCommitsInSelenium.java
@@ -5,5 +5,6 @@ public class MakingCommitsInSelenium {
         System.out.println("I am practicing github by adding selenium files ");
         System.out.println("local repo is published to github");
         System.out.println("add more work");
+        System.out.println("listening to github class and taking notes");
     }
 }
